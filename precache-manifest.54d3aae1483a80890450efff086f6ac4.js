5ec115da9999a16b9bc866b230c6cc12e34b2391
carlos iguaro <carlos.iguaro@eyss.io>
2020-11-04 20:26:43 -0400

    test router

---
+++ precache-manifest.54d3aae1483a80890450efff086f6ac4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d267efb2c373d6ab07f4a8163df5e48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50008e028e3a320d2dff",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4528340244ef53858343",
+    "url": "/static/js/2.bd59f984.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bd59f984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50008e028e3a320d2dff",
+    "url": "/static/js/main.d44ff760.chunk.js"
+  },
+  {
+    "revision": "28c89a97fd919bc448d0",
+    "url": "/static/js/runtime-main.40ff8a99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
